1c397a95fdc5980536168352ef71c018444d6a8d
marwan <marwan.benali@student.vinci.be>
2023-09-24 23:50:48 +0200

    1.1 API : read.

--- exercices/1.1/app.js
+++ exercices/1.1/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pizzaRouter = require('./routes/pizzas');
 
 var app = express();
 
