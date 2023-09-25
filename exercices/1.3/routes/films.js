54d4afe26b38e6e01ac2a70bcf609b20483d87a2
Marwan Benali El Manichi <marwan.benali@student.vinci.be>
2023-09-25 15:45:16 +0200

    initialisation pour post

--- exercices/1.3/routes/films.js
+++ exercices/1.3/routes/films.js
@@ -53,4 +53,28 @@ router.get('/:id', (req, res) => {
     res.json(films[indexOfFilmsFound]);
   });
 
+  // Create a films.
+router.post('/', (req, res) => {
+    const title = req?.body?.title?.trim()?.length !== 0 ? req.body.title : undefined;
+    const link = req?.body?.content?.trim().length !== 0 ? req.body.link : undefined;
+  
+    console.log('POST /pizzas');
+  
+    if (!title || !content) return res.sendStatus(400); // error code '400 Bad request'
+  
+    const lastItemIndex = MENU?.length !== 0 ? MENU.length - 1 : undefined;
+    const lastId = lastItemIndex !== undefined ? MENU[lastItemIndex]?.id : 0;
+    const nextId = lastId + 1;
+  
+    const newPizza = {
+      id: nextId,
+      title: title,
+      content: content,
+    };
+  
+    MENU.push(newPizza);
+  
+    res.json(newPizza);
+  });
+
 module.exports = router;
