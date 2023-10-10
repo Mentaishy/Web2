3b0051b8936aede883fb881a758f98c181fcad72
Marwan Benali El Manichi <marwan.benali@student.vinci.be>
2023-10-10 09:08:11 +0200

    2.3.1 : event object & form.

--- exercices/2.3.1/index.js
+++ exercices/2.3.1/index.js
@@ -1,2 +1,14 @@
 const form = document.querySelector('form');
 const wish = document.querySelector('#wish');
+const messageDiv = document.querySelector('#message')
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    form.style.display = 'none';
+
+    messageDiv.innerText = `Your current wish is :
+    ${wish.value}`;
+
+})
