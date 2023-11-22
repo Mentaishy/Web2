82ba23aba94e997ae743bfb5b4a0a239d3018f9d
marwan <marwan.benali@student.vinci.be>
2023-11-22 18:33:24 +0100

    2.13 : canvas event management.

--- exercices/2.13/src/Components/Pages/HomePage.js
+++ exercices/2.13/src/Components/Pages/HomePage.js
@@ -3,6 +3,7 @@ import { renderHeaderTitle } from '../../utils/render';
 const EXPECTED_RECT_COUNT = 101;
 const DEFAULT_SHAPE_SIDE = 20;
 const DEFAULT_COLOR = 'blue'; // 'rgba(255,0,0,0.5)';
+const SIZE_INCREMENT = 10;
 
 /**
  * WARNING : THE STROBOSCOPIC EFFECT OF THIS DEMO COULD LEAD TO EPILEPSY !!!
@@ -15,12 +16,23 @@ const HomePage = () => {
   const mainHeight = main.clientHeight;
   let canvas;
   let canvasContext;
+  let animationReference;
+  let animated = true;
+  let size = DEFAULT_SHAPE_SIDE;
+  let timer;
+  let animatedElementColor = DEFAULT_COLOR;
 
   renderHeaderTitle('Canvas Animation');
   renderCanvasWrapper();
   setCanvasContextAndSize();
   removePotentialVerticalAndHorizontalScrollbars();
-  requestAnimationFrame(drawOneFrame);
+  animationReference = requestAnimationFrame(drawOneFrame);
+
+  document.addEventListener('keydown', onKeyPressed);
+
+  canvas.addEventListener('click', onCanvasClick);
+
+  main.addEventListener('contextmenu', onRightClick);
 
   function renderCanvasWrapper() {
     main.innerHTML = '<canvas />';
@@ -47,11 +59,8 @@ const HomePage = () => {
     clearFrame();
 
     drawRectanglesAtRandomLocations();
-    // drawAlwaysFullRectanglesAtRandomLocations();
 
-    requestAnimationFrame(drawOneFrame);
-
-    // requestAnimationFrame(() => setTimeout(drawOneFrame, 1000));
+    animationReference = requestAnimationFrame(drawOneFrame);
   }
 
   function clearFrame() {
@@ -59,37 +68,49 @@ const HomePage = () => {
   }
 
   function drawRectanglesAtRandomLocations() {
-    canvasContext.fillStyle = DEFAULT_COLOR;
+    canvasContext.fillStyle = animatedElementColor;
 
     for (let i = 0; i < EXPECTED_RECT_COUNT; i += 1) {
-      canvasContext.fillRect(
-        Math.random() * mainWidth,
-        Math.random() * mainHeight,
-        DEFAULT_SHAPE_SIDE,
-        DEFAULT_SHAPE_SIDE,
-      );
+      canvasContext.fillRect(Math.random() * mainWidth, Math.random() * mainHeight, size, size);
     }
   }
 
-  /* function drawAlwaysFullRectanglesAtRandomLocations() {
-    canvasContext.fillStyle = DEFAULT_COLOR;
-
-    for (let i = 0; i < EXPECTED_RECT_COUNT; i += 1) {
-      const randomX = Math.random() * mainWidth;
-      let expectedX = randomX;
-      if (randomX < DEFAULT_SHAPE_SIDE) expectedX = 0;
-      else if (randomX > mainWidth - DEFAULT_SHAPE_SIDE) expectedX = mainWidth - DEFAULT_SHAPE_SIDE;
-
-      const randomY = Math.random() * mainHeight;
-      let expectedY = randomY;
-      if (randomY < DEFAULT_SHAPE_SIDE) expectedY = 0;
-      else if (randomY > mainHeight - DEFAULT_SHAPE_SIDE) {
-        expectedY = mainHeight - DEFAULT_SHAPE_SIDE;
-      }
-
-      canvasContext.fillRect(expectedX, expectedY, DEFAULT_SHAPE_SIDE, DEFAULT_SHAPE_SIDE);
+  function onKeyPressed(e) {
+    switch (e.code) {
+      case 'NumpadAdd':
+        e.preventDefault();
+        size += SIZE_INCREMENT;
+        break;
+      case 'NumpadSubtract':
+        e.preventDefault();
+        size = size > SIZE_INCREMENT ? size - SIZE_INCREMENT : 1;
+        break;
+      default:
+        break;
     }
-  } */
+  }
+
+  function onCanvasClick() {
+    animated = !animated;
+    if (!animated) {
+      cancelAnimationFrame(animationReference);
+      clearTimeout(timer);
+    } else {
+      animationReference = requestAnimationFrame(drawOneFrame);
+    }
+  }
+
+  function onRightClick(e) {
+    e.preventDefault(); // stop the right click to "bubble" and to print the contextual menu
+    animatedElementColor = getRandomRgbaColorAsString();
+  }
 };
+
+function getRandomRgbaColorAsString() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgba(${red},${green},${blue},1)`;
+}
 
 export default HomePage;
